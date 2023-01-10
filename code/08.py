a7d8c8fc509bae361a747a7faca2f87c31ba122a
Erik Streithorst <erik.streithorst@klm.com>
2023-01-10 17:54:19 +0100

    day 8 attempt

--- code/08.py
+++ code/08.py
@@ -4,7 +4,7 @@ from typing import List
 treetop_file = f"{Path(__file__).parent.parent}\\data\\08_treetop_house.txt"
 
 
-def get_rows_and_cols(tree_grid: List[str]) -> tuple[List[List[str]], List[List[str]]]:
+def get_rows_and_cols(tree_grid: List[str]):
     tree_r = []
     tree_c = [None] * len(tree_grid[0])
     first_row = True
@@ -21,10 +21,30 @@ def get_rows_and_cols(tree_grid: List[str]) -> tuple[List[List[str]], List[List[
     return tree_r, tree_c
 
 
+def is_visible(tree_line: List[str], tree_idx: int) -> bool:
+    if tree_idx == 0 or tree_idx == len(tree_line) - 1:
+        return True
+    perspective_one = tree_line[tree_idx] > max(tree_line[:tree_idx])
+    perspective_two = tree_line[tree_idx] > max(tree_line[tree_idx+1:])
+    return perspective_one or perspective_two
+
+
+def get_visibility_grid(rows: List[List[str]], cols: List[List[str]]):
+    visibility = []
+    for x in range(len(cols)):
+        visibility.append([])
+        for y in range(len(rows)):
+            visibility[x].append(is_visible(rows[y], x) or is_visible(cols[x], y))
+
+    return visibility
+
+
 with open(treetop_file, 'r') as f:
     trees = f.read().splitlines()
 
     tree_rows, tree_cols = get_rows_and_cols(trees)
+
+    visibility_grid = get_visibility_grid(tree_rows, tree_cols)
 
     print(f"Part 1: {0}")
 
